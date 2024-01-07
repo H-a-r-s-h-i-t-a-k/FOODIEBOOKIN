dd8d0fc4875c8393bf8fdd9a3c40333c8065d2e8
HARSHITA-1.0 <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-01-07 13:44:45 +0530

    Update image_str.dart

--- lib/src/constants/image_str.dart
+++ lib/src/constants/image_str.dart
@@ -1,4 +1,4 @@
-const String mSplashTopIcon = "assets/images/splash_img/splashtopicon.png";
+//const String mSplashTopIcon = "assets/images/splash_img/splashtopicon.png";
 const String mWelcomeimg = "assets/images/welcome_img/girlphone1.png";
 const String mSplashbottomIcon = "assets/images/splash_img/circle.png";
 const String mWelcomeimg2 = "assets/images/welcome_img/eatenoodles.png";
