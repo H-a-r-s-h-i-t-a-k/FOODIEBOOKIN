070da8cdb7d61e8ce0f0443b91fab2193e71b217
HARSHITA-1.0 <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-01-10 15:23:39 +0530

    Update sizes.dart

--- lib/src/constants/sizes.dart
+++ lib/src/constants/sizes.dart
@@ -1,6 +1,6 @@
 const mDefaultSize = 33.0;
 const mSplashContainerSize = 30.0;
-const mButtonHeight = 15.0;
+const mButtonHeight = 16.0;
 const mformHeight = 10.0;
 
 const mDashboardPadding = 20.0;
