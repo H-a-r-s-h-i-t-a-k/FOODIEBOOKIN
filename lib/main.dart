60874ab7ac78cad3770adb31257191e6fe9ebf5f
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-02-16 16:22:15 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:demo/firebase_options.dart';
+drimport 'package:demo/firebase_options.dart';
 import 'package:demo/src/features/auth/screens/splash/splash.dart';
 import 'package:demo/src/features/cart/controlers/blocs/cart_bloc.dart';
 import 'package:demo/src/features/home/screen/home/Bottom.dart';
@@ -10,7 +10,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:get/route_manager.dart';
 import 'src/features/cart/controlers/blocs/cart_event.dart';
-////////////
+/////
 // Gradient lgradiant = RadialGradient(
 //   radius: 3.0,
 //   // focalRadius: 4.0,
