4f0fa15a7ab496c8aacd658f585342435aee1c8b
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2024-09-04 12:31:38 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,6 @@ import 'src/features/cart/controlers/blocs/cart_event.dart';
 //     Color.fromARGB(255, 6, 230, 246),
 //     Color.fromARGB(255, 0, 0, 0)
 //   ],
-
-
 //   stops: [0.0, 0.4, 1.0],
 // );
 // Gradient dgradiant = RadialGradient(
